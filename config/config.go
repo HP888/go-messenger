e25c15196de2b27c9c64edb0451c60028210ee30
HP888 <hp888@o2.pl>
2021-04-28 17:27:19 +0200

    Added saving default configuration file when doesn't exist.

--- config/config.go
+++ config/config.go
@@ -1,6 +1,11 @@
 package config
 
-import "github.com/pelletier/go-toml"
+import (
+	"fmt"
+	"github.com/pelletier/go-toml"
+	"os"
+	"path/filepath"
+)
 
 type ServerConfig struct {
 	Host string
@@ -12,8 +17,40 @@ type Config struct {
 }
 
 func Load() Config {
+	configPath, err := filepath.Abs("config.toml")
+	if err != nil {
+		fmt.Println("Cannot resolve absolute path to configuration file!")
+		panic(err)
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		file, err := os.Create(configPath)
+		if err != nil {
+			fmt.Println("Cannot create configuration file!")
+			panic(err)
+		}
+
+		defer file.Close()
+
+		_, err = fmt.Fprintf(file,
+			"#################################\n"+
+				"#          GoMessenger\n"+
+				"#          Version 0.1\n"+
+				"#################################\n"+
+				"\n"+
+				"[server]\n"+
+				"host = '0.0.0.0'\n"+
+				"port = 9999",
+		)
+
+		if err != nil {
+			fmt.Println("Cannot save configuration file!")
+			panic(err)
+		}
+	}
+
 	config := Config{}
-	tomlConfig, _ := toml.LoadFile("config.toml")
+	tomlConfig, _ := toml.LoadFile(configPath)
 	_ = toml.Unmarshal([]byte(tomlConfig.String()), &config)
 	return config
 }
